525918d6e38d3c531d4621ca62cb3c6d6f9d6788
zeus-ctrl <74030335+zeus-ctrl@users.noreply.github.com>
2021-02-08 15:46:57 -0600

    Revert "Feedback"

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { Component } from '@angular/core';
+
 import { products } from '../products';
 
 @Component({
@@ -12,9 +12,5 @@ export class ProductListComponent {
 
   share(): void {
     window.alert('The product has been shared!');
-  }
-
-  onNotify() {
-    window.alert('You will be notified when the product goes on sale');
   }
 }
